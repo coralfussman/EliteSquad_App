f7e62157bf871182117509a18e3bd462201540da
github username <github email address>
2020-04-14 16:47:44 -0400

    added working CCI

---
+++ bin/run.rb
@@ -0,0 +1,5 @@
+require_relative '../config/environment'
+
+puts "Please enter the name you would like to go by."
+name = gets.chomp
+puts "Hi #{name}. Welcome to EliteSquad! Here you will find many different clubhouses, each with various activities and amenities that you may find of interest."
--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,8 @@ ActiveRecord::Base.establish_connection(
   database: "db/development.sqlite3"
 )
 
-ActiveRecord::Base.logger = Logger.new(STDOUT)
+# ActiveRecord::Base.logger = Logger.new(STDOUT) #comment in for logger in console
+ ActiveRecord::Base.logger = nil
+
 
 require_all 'app'

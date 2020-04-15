05b3be99f39977656e445aeb7e82abcd251976c6
github username <github email address>
2020-04-15 11:52:47 -0400

    user age working

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,20 @@
+require 'pry'
 class Member < ActiveRecord::Base
     belongs_to :record
     has_many :clubhouse, through: :record
 
+    def self.get_user_age
+        puts "Please enter your age."
+        @age = gets.chomp
+        @age = @age.to_i
+        if @age >= 21
+            puts "Your account has been created."
+        else
+            puts "You must be above 21 to use this app."
+        end 
+        @age
+    end
+
+    
 
 end
--- bin/run.rb
+++ bin/run.rb
@@ -1,16 +1,10 @@
 require_relative '../config/environment'
 
-puts "Please enter the name you would like to go by."
-name = gets.chomp
-puts "Please enter your age."
-age = gets.chomp
-if age.instance_of?(Fixnum) && (age < 99)
-    puts "Your account has been created."
-elsif age.instace_of?(Fixnum) && (age < 21)
-    puts "You must be above 21 to use this app."
-elsif
-    puts "Please enter a valid age."
-end
+Member.get_user_age
+
+
+
+
 
     
 

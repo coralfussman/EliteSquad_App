0c7a3da4963447f2bf914922302f6986ff2c183c
github username <github email address>
2020-04-15 15:00:10 -0400

    exit screen working

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,47 +3,56 @@ class Member < ActiveRecord::Base
     belongs_to :record
     has_many :clubhouse, through: :record
     
-    def self.greeting_menu
-
+    def self.greeting
         puts <<-Greeting
-                    Welcome to the Elite Squad App.          
-          Would you like to sign in or create a new profile? 
-         ____________________________________________________
+        Welcome to the Elite Squad App.          
+        Would you like to sign in or create a new profile? 
+        ____________________________________________________
         |    Sign in    |    Create Profile    |    Exit    |
         |_______________|______________________|____________|
         Greeting
+    end
 
-        input = gets.chomp
-        if input.downcase == "sign in"
-
-            puts "Please enter your username."
-            
-            loop do 
-                user_input = gets.chomp
-                if !Member.find_by(name: user_input)
-                    puts "Username not recognized. Please try again."
-                    puts "Enter 'Create Profile' if you are trying to make a new profile. " 
-                elsif user_input.downcase == 'create profile'
-                    Member.create_profile
-                    break
-                else
-                    member_info = Member.find_by(name: user_input.downcase)
-                    break
-                end
+    def self.check_username
+        loop do 
+            user_input = gets.chomp
+            if !Member.find_by(name: user_input) && !user_input.downcase == 'create profile'
+                puts "Username not recognized. Please try again."
+                puts "Enter 'Create Profile' if you are trying to make a new profile. " 
+            elsif user_input.downcase == 'create profile'
+                Member.create_profile
+                break
+            else
+                Member.find_by(name:user_input)
+                break
             end
+        end
+    end
+    
+    def self.get_username
+        
+    end
 
-            $user_session_id = member_info.id
-            puts "Welcome, #{member_info.name}."
-
-        elsif input.downcase == "create profile"
-            Member.create_profile
-        elsif input.downcase == "exit"
-            puts <<-thanks
+    def self.thank_you_exit
+        puts <<-thanks
              ___________________________________________ 
             |  Thank you for using the Elite Squad App. |
             |         We hope to see you soon!          |
             |___________________________________________|
-            thanks
+        thanks
+    end
+
+    def self.greeting_menu
+        Member.greeting
+        input = gets.chomp
+        if input.downcase == "sign in"
+            puts "Please enter your username."
+            Member.check_username
+            puts "Welcome, #{self.name}."
+        elsif input.downcase == "create profile"
+            Member.create_profile
+        elsif input.downcase == "exit"
+            Member.thank_you_exit
         end
     end
 
@@ -67,10 +76,9 @@ class Member < ActiveRecord::Base
     end
 
     def self.create_profile
-
         name = Member.create_username
         age = Member.get_user_age
-        user = self.create(name: name, age: age, tier: "bronze")
+        user = Member.create(name: name, age: age, tier: "bronze")
     end
 
     def self.create_username
@@ -80,10 +88,10 @@ class Member < ActiveRecord::Base
             if Member.get_all_usernames.include?(name)
                 puts "This username is already taken."
                 puts "Please enter a different username."
-            else
-                break
             end
+            break
         end
+        name
     end
 
     

70fe4795a9b7aabdb9f8fa6471e070dc8c5bc7e6
github username <github email address>
2020-04-15 14:12:42 -0400

    Working create profile and exit butotn

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,30 +5,48 @@ class Member < ActiveRecord::Base
     
     def self.greeting_menu
 
-        puts "Hi, welcome to the Elite Squad App."
-        puts "Would you like to sign in or create a new profile?"
-        puts " ____________________________________________________"
-        puts "|    Sign in    |    Create Profile    |    Exit    |"
-        puts "|_______________|______________________|____________|"
-        
+        puts <<-Greeting
+                    Welcome to the Elite Squad App.          
+          Would you like to sign in or create a new profile? 
+         ____________________________________________________
+        |    Sign in    |    Create Profile    |    Exit    |
+        |_______________|______________________|____________|
+        Greeting
+
         input = gets.chomp
         if input.downcase == "sign in"
+
             puts "Please enter your username."
-            username = gets.chomp
-            member_info = Member.find_by(name: username)
-            $id = member_info.id
+            
+            loop do 
+                user_input = gets.chomp
+                if !Member.find_by(name: user_input)
+                    puts "Username not recognized. Please try again."
+                    puts "Enter 'Create Profile' if you are trying to make a new profile. " 
+                elsif user_input.downcase == 'create profile'
+                    Member.create_profile
+                    break
+                else
+                    member_info = Member.find_by(name: user_input.downcase)
+                    break
+                end
+            end
+
+            $user_session_id = member_info.id
             puts "Welcome, #{member_info.name}."
+
         elsif input.downcase == "create profile"
-            puts "Create profile."
+            Member.create_profile
         elsif input.downcase == "exit"
-            puts "Thank you for using the Elite Squad App."
-            puts "We hope to see you soon."
+            puts <<-thanks
+             ___________________________________________ 
+            |  Thank you for using the Elite Squad App. |
+            |         We hope to see you soon!          |
+            |___________________________________________|
+            thanks
         end
     end
-    
-    def self.get_username
 
-    end
 
     def self.get_user_age
         puts "Please enter your age."
@@ -40,6 +58,27 @@ class Member < ActiveRecord::Base
             puts "You must be above 21 to use this app."
         end 
         @age
+    end
+
+    def self.get_all_usernames
+        Member.all.map do |x|
+            x.name 
+        end
+    end
+
+    def self.create_profile
+        puts "Please create a username."
+        loop do
+            name = gets.chomp
+            if Member.get_all_usernames.include?(name)
+                puts "This username is already taken."
+                puts "Please enter a different username."
+            else
+                break
+            end
+        end
+        age = Member.get_user_age
+        user = self.create(name: name, age: age, tier: "bronze")
     end
 
     

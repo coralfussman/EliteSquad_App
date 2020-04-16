6345c73b5458c89e26091e7de0d573b12171caaa
coralfussman <coralfussman@gmail.com>
2020-04-16 11:14:19 -0400

    commited fixes

--- bin/run.rb
+++ bin/run.rb
@@ -4,10 +4,14 @@ require_relative '../config/environment'
 # name = gets.chomp
 # puts "Hi #{name}. Welcome to EliteSquad! Here you will find many different clubhouses, each with various activities and amenities that you may find of interest."
 
-Member.greeting_menu
+greeting_menu
 
+<<<<<<< HEAD
 Member.clubhouses_with_members_tier
 #Clubhouse.clubhouse_member_options
+=======
+# Clubhouse.clubhouse_member_options
+>>>>>>> 1fd8533047e39864c7f521fe3198051bbd02b73b
 
 
 
